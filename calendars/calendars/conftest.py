7d032d61c6817b934b2099ae1f3d66c11ed2e7ae
nippi-29 <pipimelody.1726@gmail.com>
2020-10-02 11:47:47 +0900

    Initial commit

---
+++ calendars/calendars/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from calendars.users.models import User
+from calendars.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
